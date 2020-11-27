cc1dcf5f176495d91bbe04b292fe920b61079963
Qing Li <dylan.liqing@gmail.com>
2020-11-26 20:14:55 -0800

    update

--- bin/hint.py
+++ bin/hint.py
@@ -10,9 +10,15 @@ from dreamcoder.utilities import numberOfCPUs
 
 if __name__ == '__main__':
     args = commandlineArguments(
-        enumerationTimeout=50, activation='tanh', iterations=10, recognitionTimeout=3600,
-        a=3, maximumFrontier=10, topK=2, pseudoCounts=30.0,
+        enumerationTimeout=200, activation='tanh', iterations=1, recognitionTimeout=3600,
+        a=3, maximumFrontier=5, topK=2, pseudoCounts=30.0,
         helmholtzRatio=0.5, structurePenalty=1.,
         CPUs=numberOfCPUs(),
         extras=list_options)
+    args['noConsolidation'] = True
+    args['contextual'] = True
+    args['biasOptimal'] = True
+    args['auxiliaryLoss'] = True
+    args['activation'] = "relu"
+    args['useDSL'] = False
     main(args)
